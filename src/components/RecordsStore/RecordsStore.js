bbc3cf732a5d7b7e746334db00985538bab66e5c
RamS24 <AlSaadi.Rami24@gmail.com>
2022-11-15 15:25:13 +0100

    update link

--- src/components/RecordsStore/RecordsStore.js
+++ src/components/RecordsStore/RecordsStore.js
@@ -12,12 +12,11 @@ const RecordsStore = () => {
       <h1 className='animate-bounce'>Our Records Store</h1>
       <p className='animate-ping'>Welcome! Please Come In!!!</p>
     </a>
-    <a href='https://irtemallrecords.netlify.app/' title='Album search by Thili' className='text-reset text-center'>
+    <a href='https://main--music-record-finder.netlify.app/' title='Album search by Thili' className='text-reset text-center'>
       <h1 className='animate-bounce'>Search our Albums</h1>
       <p className='animate-ping'>Start Searching!!!</p>
     </a>
     </main>
-    <a href=""></a>
     <Footer page="Landing" />
     </>
   )
