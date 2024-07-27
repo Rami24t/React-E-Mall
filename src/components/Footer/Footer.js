3af76b13ca26ab59e58290eeb6fcd8d7279f9baf
Rami <alsaadi.rami24@gmail.com>
2024-07-27 13:06:38 +0200

    fix: Footer.js - remove empty className

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -144,7 +144,7 @@ const Footer = ({ page }) => {
           </div>
         </section>
 
-        <section className="">
+        <section>
           <MDBContainer className="text-center text-md-start mt-5">
             <MDBRow className="mt-3">
               <MDBCol md="3" lg="4" xl="3" className="mx-auto mb-4">
